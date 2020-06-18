9fc5b5c7f1e32011bcbd8c4c73f1adee4b49d651
Niels Lam <hoi@niels.fun>
2020-06-18 14:56:07 +0200

    Disable select if only one option is available

--- src/views/templates/inputs/select-input.template.js
+++ src/views/templates/inputs/select-input.template.js
@@ -2,7 +2,7 @@ module.exports = (data) =>
 `
 <div class="monster-configurator__group">
     <label for="${data.name}">${data.label}</label>
-    <select class="monster-configurator__input" id="${data.name}">
+    <select class="monster-configurator__input" id="${data.name}" ${data.options.length == 1 ? 'disabled':''}>
         ${data.options.map((item) => `
             <option value="${item.value}" ${data.value == item.value ? 'selected': ''}>${item.name}</option>
         `)}
