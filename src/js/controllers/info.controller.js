d2b5e960f9523e53e2f2786cf813c2de77b06c76
Niels Lam <hoi@niels.fun>
2020-06-21 18:31:23 +0200

    Remove console log

--- src/js/controllers/info.controller.js
+++ src/js/controllers/info.controller.js
@@ -3,12 +3,10 @@ import InfoView from "../views/info.view";
 export default class InfoController {
     constructor(parentController) {
         this.parentController = parentController;
-
         this.view = new InfoView(this);
     }
 
     setMonster(monster) {
-        console.log(monster);
         this.view.monster = monster;
     }
 }
